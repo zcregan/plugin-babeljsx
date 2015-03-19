32a05cda27dd6eafdac4f7efb0ba9b21b9b44f85
Zach Cregan <zcregan@outlook.com>
2015-03-19 02:39:39 -0400

    enable ES6 arrow functions temporarily

--- jsx.js
+++ jsx.js
@@ -1,7 +1,7 @@
 import babel from 'babel'
 
 export function translate (load) {
-	let output = babel.transform(load.source, {whitelist: ['react'], sourceMap: true});
+	let output = babel.transform(load.source, {whitelist: ['react', 'es6.arrowFunctions'], sourceMap: true});
 	load.source = output.code;
 	load.metadata.sourceMap = output.map;
 }
